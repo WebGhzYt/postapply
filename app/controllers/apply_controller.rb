df37dea67a79c94bf7779ed21fbf33ad4660e7d0
Rohit <engrohitjain5@gmail.com>
2015-02-03 18:03:46 -0800

    Apply with Cancel But Cancel Not Work till now porper way

--- app/controllers/apply_controller.rb
+++ app/controllers/apply_controller.rb
@@ -7,31 +7,32 @@ class ApplyController < ApplicationController
       #@post = Post.new(post_params)    # Not the final implementation!
 #   #   before_action 	:      user_signed_in?\
 
-  def create
+    def create
     @current_user = current_user
+
     @applyed_user = Apply.find_by(user_id: current_user.id, post_id: params[:post_id])
     if @applyed_user == nil
-	    @apply = Apply.new(:user_id => current_user.id, :post_id => params[:post_id],:apply => true)
-	    if @apply.save
-	      flash[:success] = "Applyed successful"
-     			#mail sending code 
-        ApplyMailer.welcome_email(@current_user).deliver
-        #@posted_user = "engrohitjain5@gmail.com"
-        @posted_user = "engrohitjain5@gmail.com"
+        @apply = Apply.new(:user_id => current_user.id, :post_id => params[:post_id],:apply => true,:status => 'applyed')
+        if @apply.save
+          flash[:success] = "Applyed successful"
+            #mail sending code 
+          ApplyMailer.welcome_email(@current_user).deliver
+          #@posted_user = "engrohitjain5@gmail.com"
+          @posted_user = "engrohitjain5@gmail.com"
           @posts  = Post.all
           @users = User.all
           @posts.each do |post|
-            if @apply.post_id == post.id
-                @sender_poster_id = post.user_id
-                @users.each do |user|
-                if @sender_poster_id == user.id
-                  @posted_user = user
-                  break
-                end
-            end
+          if @apply.post_id == post.id
+             @sender_poster_id = post.user_id
+             @users.each do |user|
+             if @sender_poster_id == user.id
+                @posted_user = user
+                break
+             end
           end
-
-     end
+        end
+      end
+      
       ApplyMailer.emp_email(@posted_user).deliver
       redirect_to posts_path
         #@posts.each do |post|
@@ -42,20 +43,25 @@ class ApplyController < ApplicationController
         
     
        else
-		   	flash[:success] = "Could Not Apply Try again"
-	      redirect_to posts_path
-	    end
-  	else
-  		  flash[:success] = "Alerday Applied"
-  		  redirect_to posts_path		
-  	end
+        flash[:success] = "Could Not Apply Try again Later"
+        redirect_to posts_path
+      end
+    else
+        flash[:success] = "Alerday Applied"
+        redirect_to posts_path    
+    end
   end
+
   def destroy
-    
+        Apply.find(params[:id]).destroy
+          flash[:success] = "Job Cancel"
+          redirect_to posts_path
+        
   end
 
 private
   def work
       
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,20 +8,20 @@ has_many :post, dependent: :destroy
 #self.per_page = 10
 has_many :active_apply, class_name: "applies",foreign_key: "user_id",
                                   dependent:   :destroy
-has_many :applying, through: :active_apply, source: :applyed
+has_many :applying, through: :active_apply, source: :post
 
 devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :confirmable, :validatable
 
 
-def apply(post)
-    active_apply.create(post_id: post.id)
-end
+# def apply(post)
+#     active_apply.create(post_id: post.id)
+# end
 
-  # Unapply a user.
-def unapply(post)
-    active_apply.find_by(post_id: post.id).destroy
-end
+#   # Unapply a user.
+# def unapply(post)
+#     active_apply.find_by(post_id: post.id).destroy
+# end
 
   # Returns true if the current user is Applying the Post.
 def applying?(post,user)
